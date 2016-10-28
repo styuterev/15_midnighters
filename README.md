6e6723735f0a4e489f7eef4f5695c5018b63bad9
sergey.tyuterev <sergey.tyuterev@datadvance.net>
2016-10-28 12:21:17 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 15_midnighters
+
+The program finds Devman users who sent their solutions at night. The term night is defined here as the time between midnight and dawn, where dawn can be defined by user and is 5 AM by default. It only looks for users who submitted their attempts not so long ago, as the API only allows to access 10 pages of logs.   
+
+Dawn time is the only positional integer argument. 5 by default.   
+
+### How to use
+
+> python seek_dev_nighters.py   
+Find all users made an attempt between 12 AM and 5 AM   
+
+> python seek_dev_nighters.py 6   
+Find all users made an attempt between 12 AM and 6 AM   
+
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,66 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import argparse
+import datetime
+import pytz
+import requests
 
-def get_midnighters():
-    pass
+
+def load_attempts():  # Currently, some attempts are lost because API only provides 10 pages.
+    """
+    Retrieves all attempts made by Devman users as far back as the API keeps track.
+    :returns a generator of attempts
+    """
+    numeration_shift = 1
+    base = 'https://devman.org/api/challenges/solution_attempts/'
+    base_response_json = requests.get(base).json()
+    number_of_pages = base_response_json['number_of_pages']
+    for page in range(numeration_shift, number_of_pages + numeration_shift):
+        payload = {'page': page}
+        response_json = requests.get(base, params=payload).json()
+        for record in response_json['records']:
+            yield record
+
+
+def get_owls(records, dawn_time=5):
+    """
+    From the given records, gets usernames of users who made their attempts between
+    midnight and dawn_time, where dawn_time can be set as a parameter.
+    :returns a generator of usernames
+    """
+    for record in records:
+        attempt_time = get_local_time(record)
+        if attempt_time is None:
+            continue
+        midnight = attempt_time.replace(hour=0, minute=0, second=0, microsecond=0)
+        dawn = attempt_time.replace(hour=dawn_time, minute=0, second=0, microsecond=0)
+        if midnight < attempt_time < dawn:
+            yield record['username']
+
+
+def get_local_time(record):
+    """
+    From a given attempt record, gets a local time of the attempt,
+    based on the site's time and user's timezone.
+    :returns local time of the attempt
+    """
+    if record['timestamp'] is None or record['timezone'] is None:
+        return None
+    naive_time = datetime.datetime.utcfromtimestamp(record['timestamp'])
+    aware_time = pytz.utc.localize(naive_time)
+    local_time = aware_time.astimezone(pytz.timezone(record['timezone']))
+    return local_time
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dawn_time', action='store', default=5,
+                        help='the time you want to consider the end of the night')
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-  pass
+    arguments = parse_arguments()
+    dawn_time = int(arguments.dawn_time)
+    attempts = load_attempts()
+    owl_generator = get_owls(attempts, dawn_time)
+    unique_owls = set(list(owl_generator))
+    print(unique_owls)
